f9503d067ad4a86946289fade4090b61033fd1e1
vineet kumar <vineetabacus@gmail.com>
2024-10-29 17:19:05 +0530

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+
+def test_read_item():
+    response = client.get("/items/42?q=test")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 42, "q": "test"}
